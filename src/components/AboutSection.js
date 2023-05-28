0072d4a8bb45e295067be33f3176b15b70b7e743
Andrew Mamchyn <mamchinn@gmail.com>
2023-05-27 21:44:20 -0400

    styled why choose us section, seperated global styles into styles.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,12 @@ import React from 'react';
 import home1 from "../img/home1.png"
 // Styled
 import styled from "styled-components";
+import {
+    StyledAbout,
+    StyledDescription,
+    StyledImage,
+    StyledHide
+} from "../styles";
 
 function AboutSection(props) {
     return (
@@ -31,38 +37,4 @@ function AboutSection(props) {
         </StyledAbout>
     );
 }
-
-// Styled Component
-
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`
-
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2{
-    font-weight: lighter;
-  }
-`
-
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img{
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`
-
-const StyledHide = styled.div`
-  overflow: hidden;  
-`
-
 export default AboutSection;
--- src/components/WhyChooseUs.js
+++ src/components/WhyChooseUs.js
@@ -6,73 +6,102 @@ import money from "../img/money.svg"
 import teamwork from "../img/teamwork.svg"
 //Import Image
 import home2 from "../img/home2.png"
+//Styles
+import {
+    StyledAbout,
+    StyledDescription,
+    StyledImage
+} from "../styles";
+import styled from "styled-components";
 
 function WhyChooseUs(props) {
     return (
-        <div className='services'>
-            <div className='description'>
+        <StyledWhyChooseUs>
+            <StyledDescription>
                 <h2>
                     High <span>quality</span> services
                 </h2>
-                <div className='cards'>
-                    <div className='card'>
+                <StyledCards>
+                    <StyledCard>
                         <div className='icon'>
                             <img src={clock} alt='clock'/>
                             <h3>Efficient</h3>
-                            <p>
-                                We capture your precious moments with stunning photography and cinematography,
-                                delivering your visual narrative with the speed of life. At our company, your
-                                memories don't wait. We pride ourselves on our swift, efficient service, ensuring
-                                that your beautifully crafted images and films are ready by the next day. Your story
-                                deserves to be told without delay. We capture today; you relive it tomorrow.
-                            </p>
                         </div>
-                    </div>
-                    <div className='card'>
+                            <p>
+                                We capture and deliver stunning photography and videography content swiftly and
+                                efficiently.
+                            </p>
+                    </StyledCard>
+                    <StyledCard>
                         <div className='icon'>
                             <img src={teamwork} alt='clock'/>
                             <h3>Teamwork</h3>
+                        </div>
                             <p>
                                 Our photography and cinematography services are powered by a team of passionate
-                                professionals. Together, we blend our talents to bring your story to life, working
-                                collaboratively to ensure every frame captures your unique vision. With us, you're not
-                                just hiring a service, you're joining a team dedicated to creating your masterpiece.
-                                Your story is our mission, and together, we create magic.
+                                professionals.
                             </p>
-                        </div>
-                    </div>
-                    <div className='card'>
+                    </StyledCard>
+                    <StyledCard>
                         <div className='icon'>
                             <img src={diaphragm} alt='diaphragm'/>
                             <h3>Creativity</h3>
-                            <p>
-                                At the heart of our photography and cinematography services lies a boundless well of
-                                creativity. We see beyond the lens, transforming ordinary moments into extraordinary
-                                visual narratives. Our approach blends technical expertise with artistic innovation
-                                to create compelling stories that stand out. Experience the magic of storytelling with
-                                us, where every frame is a testament to our creative spirit.
-                            </p>
                         </div>
-                    </div>
-                    <div className='card'>
+                            <p>
+                                We thrive on innovation and out of the box thinking, providing distinctly creative
+                                solutions that ensure every image is unique and impactful.
+                            </p>
+                    </StyledCard>
+                    <StyledCard>
                         <div className='icon'>
                             <img src={money} alt='money'/>
                             <h3>Affordable</h3>
-                            <p>
-                                Experience the magic of professional photography and cinematography without breaking
-                                the bank. Our mission is to make high-quality visual storytelling accessible to everyone.
-                                With our affordable rates, everyone can enjoy the luxury of having their precious
-                                moments beautifully captured. Extraordinary memories shouldn't come with an extravagant
-                                price tag. Capture your story with us, where quality meets affordability.
-                            </p>
                         </div>
-                    </div>
-                </div>
-            </div>
-            <img src={home2} alt='camera'/>
-        </div>
+                            <p>
+                                High-quality services tailored to your needs at budget-friendly prices, offering
+                                unbeatable value without compromising on excellence.
+                            </p>
+                    </StyledCard>
+                </StyledCards>
+            </StyledDescription>
+            <StyledImage>
+               <img src={home2} alt='camera'/>
+            </StyledImage>
+        </StyledWhyChooseUs>
     );
 }
 
+
+const StyledWhyChooseUs = styled(StyledAbout)`
+  h2{
+    padding-bottom: 5rem;
+  }
+  p{
+    width: 70%;
+    padding: 2rem 0 4rem 0;
+  }
+`
+
+const StyledCards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  @media (max-width: 1300px) {
+    justify-content: center;
+  }
+`
+
+const StyledCard = styled.div`
+  flex-basis: 20rem;
+  .icon{
+    display: flex;
+    align-items: center;
+    h3{
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`
 
 export default WhyChooseUs;
